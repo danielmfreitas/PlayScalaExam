8a576f0fb735ba660bae3811344940a80944a6ac
Daniel Freitas <daniel.mfreitas@gmail.com>
2011-11-09 01:33:11 -0500

    Removed Users controller and view package :(

--- app/controllers.scala
+++ app/controllers.scala
@@ -9,22 +9,18 @@ import com.mongodb.casbah.commons.MongoDBObject
 
 object Application extends Controller {
 
+  val mongoConnection = MongoConnection()
+  val format = new java.text.SimpleDateFormat("yyyy-MM-dd")
 
   def index = {
     views.Application.html.index("Hello You!")
   }
-}
 
-object Users extends Controller {
-  val mongoConnection = MongoConnection()
-  val format = new java.text.SimpleDateFormat("yyyy-MM-dd")
-
-  def findByUsername(username: String) {
+  def findUserByUsername(username:String) = {
     val mongoUser = mongoConnection("scalatest")("users").findOne(MongoDBObject("username" -> username)).get
     val user = new User(mongoUser.get("username").asInstanceOf[String],
       mongoUser.get("fullName").asInstanceOf[String],
       format.parse(mongoUser.get("dateOfBirth").asInstanceOf[String]))
-
-    html.hellouser("Hello You!", user)
+    views.Application.html.hellouser("Hello You!", user)
   }
 }
